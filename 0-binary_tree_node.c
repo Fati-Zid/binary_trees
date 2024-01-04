14188cc309e67f8c9195d11dcda776f87d414811
fati zid <97165021+fatimaZohraEzzaidani@users.noreply.github.com>
2024-01-04 21:13:16 +0100

    style: betty-style formating

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -8,21 +8,20 @@
  *
  * Return: A pointer to the newly created node.
  */
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value){
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	/* Allocate memory for the new node */
+	binary_tree_t *newNode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
 
-    // Allocate memory for the new node
-    binary_tree_t *newNode = (binary_tree_t*) malloc(sizeof(binary_tree_t));
+	/* Check if memory allocation was successful */
+	if (newNode == NULL)
+		return (NULL);
 
-     // Check if memory allocation was successful
-    if (newNode == NULL)
-        return NULL;
+	/* Initialize the new node */
+	newNode->n = value;
+	newNode->left = NULL;
+	newNode->right = NULL;
+	newNode->parent = parent;
 
-    // Initialize the new node
-    newNode->n = value;
-    newNode->left = NULL;
-    newNode->right = NULL;
-    newNode->parent = parent;
-
-    return newNode;
-    
- }
+	return (newNode);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -1,15 +1,34 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
-typedef struct binary_tree{
-    int n;
-    struct binary_tree* parent;
-    struct binary_tree* left;
-    struct binary_tree* right;
+/**
+ * struct binary_tree - Binary tree node structure.
+ * @n: Value of the node.
+ * @parent: Pointer to the parent node.
+ * @left: Pointer to the left child node.
+ * @right: Pointer to the right child node.
+ */
+typedef struct binary_tree
+{
+	int n;
+	struct binary_tree *parent;
+	struct binary_tree *left;
+	struct binary_tree *right;
+} binary_tree_t;
 
-}binary_tree_t;
-
+/**
+ * binary_tree_node - Creates a binary tree node.
+ * @parent: Pointer to the parent node.
+ * @value: Value to be assigned to the new node.
+ *
+ * Return: A pointer to the newly created node.
+ */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+/**
+ * binary_tree_print - Prints a binary tree.
+ * @tree: Pointer to the root node of the tree to print.
+ */
 void binary_tree_print(const binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
