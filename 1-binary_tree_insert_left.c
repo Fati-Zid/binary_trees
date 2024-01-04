3a53e4f061b33eb74735280d816922e5e7207dde
fati zid <97165021+fatimaZohraEzzaidani@users.noreply.github.com>
2024-01-04 23:15:51 +0100

    origin code 1-left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -18,13 +18,12 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (newNode == NULL)
 		return (NULL);
 
-	newNode->left = parent->left;
-	parent->left = newNode;
-
-	if (newNode->left != NULL)
+	if (parent->left != NULL)
 	{
-		newNode->left->parent = newNode;
+		newNode->left = parent->left;
+		parent->left->parent = newNode;
 	}
+	parent->left = newNode;
 
 	return (newNode);
 }
