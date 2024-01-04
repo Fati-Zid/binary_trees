4f853071e7e805cf108f06376346ceec787c8e67
fati zid <97165021+fatimaZohraEzzaidani@users.noreply.github.com>
2024-01-04 22:53:44 +0100

    header changed

--- binary_trees.h
+++ binary_trees.h
@@ -2,19 +2,22 @@
 #define BINARY_TREES_H
 
 /**
- * struct binary_tree - Binary tree node structure.
- * @n: Value of the node.
- * @parent: Pointer to the parent node.
- * @left: Pointer to the left child node.
- * @right: Pointer to the right child node.
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
  */
-typedef struct binary_tree
+struct binary_tree_s
 {
-	int n;
-	struct binary_tree *parent;
-	struct binary_tree *left;
-	struct binary_tree *right;
-} binary_tree_t;
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+};
+
+typedef struct binary_tree_s binary_tree_t;
 
 /**
  * binary_tree_node - Creates a binary tree node.
