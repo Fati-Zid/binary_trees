d75b5dc6a851a31a4372a52500784ff14706461d
fati zid <97165021+fatimaZohraEzzaidani@users.noreply.github.com>
2024-01-05 01:12:36 +0100

    traversal: in-order, post-order

--- binary_trees.h
+++ binary_trees.h
@@ -89,4 +89,21 @@ int binary_tree_is_root(const binary_tree_t *node);
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 
+/**
+ * binary_tree_inorder -  function that goes through a binary tree using in-order traversal.
+ * @tree: is a pointer to the root node of the tree to traverse.
+ * @func:is a pointer to a function to call for each node. 
+ * Return: void.
+ */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
+
+
+/**
+ * binary_tree_postorder -  function that goes through a binary tree using post-order traversal.
+ * @tree: is a pointer to the root node of the tree to traverse.
+ * @func:is a pointer to a function to call for each node. 
+ * Return: void.
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+
 #endif /* BINARY_TREES_H */
