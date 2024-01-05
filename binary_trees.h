69db1df374bc3c889e096bfe0773a40bc5301654
fati zid <97165021+fatimaZohraEzzaidani@users.noreply.github.com>
2024-01-05 01:28:22 +0100

    get: height, depth, size, leaves, nodes

--- binary_trees.h
+++ binary_trees.h
@@ -106,4 +106,38 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 
+/**
+ * binary_tree_height - Function that measures the height of a binary tree
+ * @tree: tree to go through
+ * Return: the height
+ */
+size_t binary_tree_height(const binary_tree_t *tree);
+
+/**
+ * binary_tree_depth - depth of specified node from root
+ * @tree: node to check the depth
+ * Return: 0 is it is the root or number of depth
+ */
+size_t binary_tree_depth(const binary_tree_t *tree);
+
+/**
+ * binary_tree_size - function that return the size of a tree
+ * @tree: tree to check
+ * Return: size of tree
+ */
+size_t binary_tree_size(const binary_tree_t *tree);
+
+/**
+ * binary_tree_leaves - function that returns the number of leaves in a tree
+ * @tree: tree to check
+ * Return: number of leaves
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree);
+
+/**
+ * binary_tree_nodes - function that returns # of nodes in a tree with children
+ * @tree: tree to check
+ * Return: number of nodes with children
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree);
 #endif /* BINARY_TREES_H */
